48a2e8b9f93f4e6b76c09e8aa094b723a06cc055
Juanfe008 <54733201+Juanfe008@users.noreply.github.com>
2024-12-09 21:23:45 -0500

    fix filename 2

--- src/Auth/RutaProtegida.jsx
+++ src/Auth/RutaProtegida.jsx
@@ -1,5 +1,5 @@
 import { Outlet, Navigate } from "react-router-dom";
-import { useAuth } from './auth/AuthProvider.jsx';
+import { useAuth } from "./AuthProvider";
 
 function RutaProtegida({ allowedRoles }) {
     const auth = useAuth();
